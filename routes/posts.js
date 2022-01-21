d53a6b796dd4baeffe52d1512c2b78b1911c0e2b
LongGoll <39117663+longcnniu@users.noreply.github.com>
2022-01-21 16:54:33 +0700

    upload

---
+++ routes/posts.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const router = express.Router()
+
+//Load model
+const Post = require('../models/Post')
+
+//Hien tat ca cac post
+router.get('/', async(req, res) => {
+    const posts = await Post.find().lean().sort({date: -1})
+    res.render('posts/index', {posts})
+})
+
+// Hien thi form de tao bai vie moi
+router.get('/add', (req, res) => {
+    res.render('posts/add')    
+})
+
+//tao post moi
+router.post('/', async(req, res) => {
+    const {title, text} = req.body
+
+    let errore = []
+
+    if(!title) errore.push({msg: 'Title required'})
+    if(!text) errore.push({msg: 'Text required'})
+    if(errore.length > 0 ) res.rander('posts/add', {title, text})
+    else {
+        const newPostData = {title, text}
+        const newPost = new Post(newPostData)
+        await newPost.save()
+        res.redirect('/posts')
+    }
+})
+
+//Hien thi form de nguoi dung chinh sua
+router.get('/edit/:id', async(req, res) => {
+    const post = await Post.findOne({_id: req.params.id}).lean()
+    res.render('posts/edit', {post})
+})
+
+//Cap nhan thay doi
+router.put('/:id', async(req, res) => {
+    const {title, text} = req.body
+    await Post.findOneAndUpdate({_id: req.params.id}, {title, text})
+    res.redirect('/posts')
+})
+
+//xoa bai viet
+router.delete('/:id', async(req, res) => {
+    await Post.findOneAndDelete({_id: req.params.id})
+    res.redirect('/posts')
+})
+
+module.exports = router
